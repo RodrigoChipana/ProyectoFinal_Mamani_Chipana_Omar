9b615ca1be111493c978160a28c391b6264ba588
rodrigo <rodrigochipana95@gmail.com>
2023-01-26 01:19:48 -0300

    Se agrego async, await y fetch

---
+++ js/partners.js
@@ -0,0 +1,19 @@
+//seleccionamos el id list
+const lista = document.querySelector('#list')
+
+//Implementamos async, await y fetch para recuperar datos del proyecto
+ const obtenerMarcas = async () => {
+    const resp = await
+    fetch('./js/partners.json')
+    const data = await resp.json()
+   
+    data.forEach((marca) => {
+    const li = document.createElement('li')
+    li.innerHTML = `
+    <h4>${marca.name}</h4>
+    `
+    lista.append(li)
+    })
+   }
+
+   obtenerMarcas()
